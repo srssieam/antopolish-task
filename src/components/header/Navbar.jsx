242120d75999876423272b222d81d66773b8ecf3
srs-sieam <srssieam@gmail.com>
2024-03-25 16:09:41 +0600

    complete environment setup and created navbar component

---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,27 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import "./headerStyle.css"
+const Navbar = () => {
+    return (
+        <header>
+            <div>
+                <h1 className="logo-title">Taste now</h1>
+            </div>
+            <nav>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#about-us">About us</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#contact">Contact us</a></li>
+                        <li><a href="#contact"><AiOutlineShoppingCart className="cart-icon" /></a></li>
+                        {/* <li ></li> */}
+                    </ul>
+                </nav>
+              
+                <button className="btn">sign up</button>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
